e7855a584f5ef8c05cc07ffceed6a37a83ed5bb2
falihnaufal17 <dudu.kotkot@gmail.com>
2020-06-10 20:23:03 +0700

    first init

---
+++ app.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const engines = require("consolidate");
+const paypal = require("paypal-rest-sdk");
+
+const app = express();
+
+app.engine("ejs", engines.ejs);
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+paypal.configure({
+    mode: "sandbox", //sandbox or live
+    client_id:
+        "AU1Ip9EcfJbX0Km6nWAsKKni2yFDA16HbeOp9WrsCpNrT9pEXNB_X7IpkhZE7itDVYcNTx2uaIY2vAUw",
+    client_secret:
+        "EJ7yw3uBIFAXofFwxI7PRMmru3Xt70kLHI6RgIPvW9LRXl0DKKVBU82f0X6ovDFSHzO_nxRkdPTw1dGL"
+});
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/paypal", (req, res) => {
+    var create_payment_json = {
+        intent: "sale",
+        payer: {
+            payment_method: "paypal"
+        },
+        redirect_urls: {
+            return_url: "http://localhost:3000/success",
+            cancel_url: "http://localhost:3000/cancel"
+        },
+        transactions: [
+            {
+                item_list: {
+                    items: [
+                        {
+                            name: "item",
+                            sku: "item",
+                            price: "1.00",
+                            currency: "USD",
+                            quantity: 1
+                        }
+                    ]
+                },
+                amount: {
+                    currency: "USD",
+                    total: "1.00"
+                },
+                description: "This is the payment description."
+            }
+        ]
+    };
+
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            console.log("Create Payment Response");
+            console.log(payment);
+            res.redirect(payment.links[1].href);
+        }
+    });
+});
+
+app.get("/success", (req, res) => {
+    var PayerID = req.query.PayerID;
+    var paymentId = req.query.paymentId;
+    var execute_payment_json = {
+        payer_id: PayerID,
+        transactions: [
+            {
+                amount: {
+                    currency: "USD",
+                    total: "1.00"
+                }
+            }
+        ]
+    };
+
+    paypal.payment.execute(paymentId, execute_payment_json, function (
+        error,
+        payment
+    ) {
+        if (error) {
+            console.log(error.response);
+            throw error;
+        } else {
+            console.log("Get Payment Response");
+            console.log(JSON.stringify(payment));
+            res.render("success");
+        }
+    });
+});
+
+app.get("cancel", (req, res) => {
+    res.render("cancel");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is running");
+});
+
